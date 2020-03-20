8843714d0ce0ec73d19c650b5a469796461f9165
Steven Jordan <pydtyper@gmail.com>
2020-03-20 19:23:56 -0400

    initial commit

---
+++ taosite/webpage/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+
+def home(request):
+
+    context = {}
+    return render(request, "home.html", context)
+
+
+def about(request):
+
+    context = {}
+    return render(request, "about.html", context)
+
